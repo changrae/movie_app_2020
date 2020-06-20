3aa8407e78ab2221388a287814d384be6e3a04ac
JAMES <crjang75@gmail.com>
2020-06-20 21:56:42 +0900

    Updates

---
+++ precache-manifest.3c3c56eb7a62c7ad014e99fdbcfc7310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6937dc146d3b9b2e6767cd39c3ce75d6",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "df94ce3158c16211337b",
+    "url": "/movie_app_2020/static/css/main.9fdf5d78.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df94ce3158c16211337b",
+    "url": "/movie_app_2020/static/js/main.45cc985d.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
